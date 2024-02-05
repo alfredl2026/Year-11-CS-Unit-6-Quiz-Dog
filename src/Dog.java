46f190c29b2f35040dcac4b155d729f94756e689
Alfred Lam <pakto1230@gmail.com>
2024-02-05 08:14:19 +0800

    done

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,46 @@
 public class Dog {
+    private int age;
+    private String name;
+    private String color;
+    private Owner owner;
+    public Dog(int age, String name, String color, Owner owner){
+        this.age = age;
+        this.name = name;
+        this.color = color;
+        this.owner = owner;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public Owner getOwner(){
+        return this.owner;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setOwner(Owner owner){
+        this.owner = owner;
+    }
+    private void addOneAge(){
+        this.age++;
+    }
+    public int birthday(){
+        this.addOneAge();
+        return this.age;
+    }
+    public String toString(){
+        return("The dog is named " + this.name + " and is " + this.age + " years old. This dog is " + this.color + ".");
+    }
 }
--- src/Owner.java
+++ src/Owner.java
@@ -1,2 +1,19 @@
 public class Owner {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    public Owner(String firstName, String lastName, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
 }
